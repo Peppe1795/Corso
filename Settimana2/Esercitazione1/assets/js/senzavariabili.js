e29c504520899772d306dcd5a23c523f21910c4c
Peppe1795 <petrucci238@gmail.com>
2023-04-03 16:32:59 +0200

    EsercitazioneSettimana2es1

---
+++ Settimana2/Esercitazione1/assets/js/senzavariabili.js
@@ -0,0 +1,11 @@
+alert('Aspetta sto lavorando');
+
+alert('Sono pronto VAI');
+
+document.getElementById('Output').innerHTML = 'Output scritto da Javascript';
+
+document.getElementById('Titolo').innerHTML = 'Iniziamo la nostra pratica';
+
+document.write('<p>Pantera scritto da Javascript</p>');
+
+document.write('<a href="pagina1.html" rel="follow"> Torna alla home</a>');
---
+++ Settimana2/Esercitazione1/assets/js/variabili.js
@@ -0,0 +1,8 @@
+var criceto = 'Criceto';
+console.log(criceto);
+
+var giraffa = 'Giraffa';
+console.log(giraffa);
+
+var leone = 'Leone';
+console.log(leone);
