97a5710d25153ab7f2006e3282a820394d5483e8
Peppe1795 <petrucci238@gmail.com>
2023-04-07 17:03:51 +0200

    ProgettoSettimana02

---
+++ Settimana2/ProgettoSettimana02/Javascript/assets/js/D5.js
@@ -0,0 +1,146 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+    Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
+*/
+//soluzione:
+
+const pets = ['dog', 'cat', 'hamster', 'redfish']
+for (let i = 0; i < pets.length; i++) {
+  const stampaArray = pets[i];
+  console.log(stampaArray);
+}
+
+/* ESERCIZIO 2
+    Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
+*/
+//soluzione:
+
+pets.sort();
+console.log(pets);
+
+/* ESERCIZIO 3
+    Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
+*/
+//soluzione:
+
+for (let k = pets.length - 1; k >=0; k--) {
+const invertito = pets[k];
+console.log(invertito);
+}
+
+/* ESERCIZIO 4
+    Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
+*/
+//soluzione:
+
+console.log(pets);
+pets.shift();
+pets.push('cat');
+console.log(pets);
+
+/* ESERCIZIO 5
+    Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
+*/
+const cars = [
+  {
+    brand: 'Ford',
+    model: 'Fiesta',
+    color: 'red',
+    trims: ['titanium', 'st', 'active'],
+  },
+  {
+    brand: 'Peugeot',
+    model: '208',
+    color: 'blue',
+    trims: ['allure', 'GT'],
+  },
+  {
+    brand: 'Volkswagen',
+    model: 'Polo',
+    color: 'black',
+    trims: ['life', 'style', 'r-line'],
+  },
+]
+
+//soluzione:
+
+for (let j = 0; j < cars.length; j++) {
+  const targa = cars[j];
+  targa.licensePlate = 'AZN9590';
+console.log(cars);
+}
+
+
+/* ESERCIZIO 6
+    Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
+    Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
+*/
+//soluzione:
+
+ const miaAuto = {
+  brand: 'bmw',
+  model: 'serie3',
+  color: 'black',
+  licensePlate: "AZN9590",
+  trims: ['life', 'style', 'r-line'],
+ }
+ 
+ cars.push(miaAuto);
+
+ console.log(cars);
+
+ /* ESERCIZIO 7
+    Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
+*/
+//soluzione:
+
+const justTrims = []
+
+for (let x = 0; x < cars.length ; x++ ) {
+ 
+  justTrims.push(cars[x].trims[0]);
+console.log(justTrims);
+}
+/* ESERCIZIO 8
+    Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
+    "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
+*/
+
+for (let z = 0; z < cars.length; z++) {
+  const condizione = cars[z];
+  if (condizione.color.charAt(0) == 'b') {
+    console.log('Fizz');
+  }else if(condizione.color.charAt(0) != 'b') {
+    console.log('Buzz');
+  }
+  }
+
+/* ESERCIZIO 9
+    Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
+*/
+const numericArray = [
+  6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
+]
+ var f = 0;
+
+while (f < numericArray.length - 4) {
+ console.log(numericArray[f]);
+f++;
+}
+
+
+/* ESERCIZIO 10
+    Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
+    dell'alfabeto italiano.
+    es. [f, b, e] --> [6, 2, 5]
+*/
+const charactersArray = ["g", "n", "u", "z", "d"]
+var alfabeto = 0;
+//switch ()
