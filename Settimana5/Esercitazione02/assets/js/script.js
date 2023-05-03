9bf618e31244bb3b07a4ec749186f4acd7358864
Peppe1795 <petrucci238@gmail.com>
2023-05-03 19:58:29 +0200

    Settimana5 Esercitazione02

--- Settimana5/Esercitazione02/assets/js/script.js
+++ Settimana5/Esercitazione02/assets/js/script.js
@@ -7,10 +7,22 @@ const removeCards = () => {
     item.innerHTML = ''
    })
 }
-
+//commento la funzione poichè elimina tutte le card
 //removeCards()
 
+var selezione = document.getElementsByClassName('notifica');
+var selezione2 = document.getElementsByClassName('notifica1');
 
+const badgeHot = () => {
+    for(let i = 0; i < selezione.length; i++) {
+        selezione[i].classList.add('item');
+    }
+    for(let k = 0; k < selezione2.length; k++) {
+        selezione2[k].classList.add('notify-badge');
+    }
+}
+
+badgeHot();
 
 var bottone = document.getElementById('viaggi');
 const viaggiAttvi = () => {
