c5713649edb5f6b7569300bf02857d00e234e6e2
Peppe1795 <petrucci238@gmail.com>
2023-05-10 17:55:53 +0200

    Settimana7Esercitazione03

---
+++ Settimana7/Esercitazione03/assets/js/script.js
@@ -0,0 +1,29 @@
+const bottone = document.querySelector('.cancella');
+const url = "https://striveschool-api.herokuapp.com/books";
+const riga = document.getElementById("riga1");
+const carrello = [];
+window.onload = () => {
+  fetch(url)
+    .then((object) => object.json())
+    .then((tutto) => {
+      for (let i of tutto) {
+        riga.innerHTML += ` <div class="col-12 col-md-6 col-lg-4 mb-3"><div class="card h-100 ">
+        <img src="${i.img}" class="card-img-top" alt="image book">
+        <div class="card-body">
+          <h5 class="card-title">${i.title}</h5>
+          <p class="card-text">${i.price}</p>
+          <button type="button" class="btn btn-warning p-2 cancella">SCARTA</button>
+          <button type="button" class="btn btn-warning p-2">AGGIUNGI AL CARELLO</button>
+        </div>
+      </div></div>`;
+      }
+    })
+    .catch((error) => console.log(error));
+  
+    riga.addEventListener("click", (event) => {
+    if (event.target.classList.contains('cancella')) {
+      const rimuovi = event.target.closest(".col-12");
+      rimuovi.remove();
+    }
+  });
+};
