81237a97fc06a285a94337454b869cc7fd636923
Peppe1795 <petrucci238@gmail.com>
2023-05-22 16:56:08 +0200

    Settimana9 Esercitazione01

--- Settimana9/Esercitazione01/assets/script/script.js
+++ Settimana9/Esercitazione01/assets/script/script.js
@@ -1,49 +1,47 @@
 var btn = document.getElementById("bottone");
 var input1 = document.getElementById("giocatore1");
 var input2 = document.getElementById("giocatore2");
-var giocatore1 = input1 === null || input1 === void 0 ? void 0 : input1.value;
-var giocatore2 = input2 === null || input2 === void 0 ? void 0 : input2.value;
 var estratto = document.getElementById("estratto");
 var risultato = document.getElementById("risultato");
 var resetto = document.getElementById("resetto");
 window.onload = function () {
     btn.reset();
 };
-var controlla = function () {
-    if (giocatore1 < 1 || giocatore1 > 100) {
+var controlla = function (x, y) {
+    if (x < 1 || x > 100) {
         alert("Attenzione !! Giocatore1 inserisci un numero compreso tra 1 e 100");
     }
-    else if (giocatore2 < 1 || giocatore2 > 100) {
+    else if (y < 1 || y > 100) {
         alert("Attenzione !! Giocatore2 inserisci un numero compreso tra 1 e 100");
     }
-    else if (giocatore1 === giocatore2) {
+    else if (x === y) {
         alert("Attenzione i due giocatori devoni inserire numeri diversi");
     }
-    else {
-        vincitore();
-    }
 };
-var vincitore = function () {
+var vincitore = function (x, y) {
     var randomNumer = Math.floor(Math.random() * (100 - 1) + 1);
     estratto.innerHTML = "".concat(randomNumer);
-    if (giocatore1 === randomNumer) {
+    if (x === randomNumer) {
         risultato.innerHTML = "Giocatore1 ha indovinato il numero";
     }
-    else if (giocatore2 === randomNumer) {
+    else if (y === randomNumer) {
         risultato.innerHTML = "Giocatore2 ha indovinato il numero";
     }
-    else if (Math.abs(giocatore1 - randomNumer) < Math.abs(giocatore2 - randomNumer)) {
+    else if (Math.abs(x - randomNumer) < Math.abs(y - randomNumer)) {
         risultato.innerHTML =
             "Nessuno ha indovinato ma Giocatore 1 si è avvicinato di più";
     }
-    else if (Math.abs(giocatore2 - randomNumer) < Math.abs(giocatore1 - randomNumer)) {
+    else if (Math.abs(y - randomNumer) < Math.abs(x - randomNumer)) {
         risultato.innerHTML =
             "Nessuno ha indovinato ma Giocatore 2 si è avvicinato di più";
     }
 };
 btn.addEventListener("submit", function (event) {
     event.preventDefault();
-    controlla();
+    var giocatore1 = Number(input1 === null || input1 === void 0 ? void 0 : input1.value);
+    var giocatore2 = Number(input2 === null || input2 === void 0 ? void 0 : input2.value);
+    controlla(giocatore1, giocatore2);
+    vincitore(giocatore1, giocatore2);
 });
 resetto.addEventListener("click", function () {
     estratto.innerHTML = "";
--- Settimana9/Esercitazione01/assets/script/script.ts
+++ Settimana9/Esercitazione01/assets/script/script.ts
@@ -1,43 +1,35 @@
-let btn = document.getElementById("bottone") as HTMLFormElement;
-let input1 = document.getElementById("giocatore1") as HTMLInputElement | null;
-let input2 = document.getElementById("giocatore2") as HTMLInputElement | null;
-let giocatore1: any = input1?.value;
-let giocatore2: any = input2?.value;
-let estratto = document.getElementById("estratto") as HTMLParagraphElement;
-let risultato = document.getElementById("risultato") as HTMLParagraphElement;
-let resetto = document.getElementById("resetto") as HTMLButtonElement;
+const btn = document.getElementById("bottone") as HTMLFormElement;
+const input1 = document.getElementById("giocatore1") as HTMLInputElement;
+const input2 = document.getElementById("giocatore2") as HTMLInputElement;
+const estratto = document.getElementById("estratto") as HTMLParagraphElement;
+const risultato = document.getElementById("risultato") as HTMLParagraphElement;
+const resetto = document.getElementById("resetto") as HTMLButtonElement;
 
 window.onload = () => {
   btn.reset();
 };
 
-const controlla = () => {
-  if (giocatore1 < 1 || giocatore1 > 100) {
+const controlla = (x: number, y: number) => {
+  if (x < 1 || x > 100) {
     alert("Attenzione !! Giocatore1 inserisci un numero compreso tra 1 e 100");
-  } else if (giocatore2 < 1 || giocatore2 > 100) {
+  } else if (y < 1 || y > 100) {
     alert("Attenzione !! Giocatore2 inserisci un numero compreso tra 1 e 100");
-  } else if (giocatore1 === giocatore2) {
+  } else if (x === y) {
     alert("Attenzione i due giocatori devoni inserire numeri diversi");
-  } else {
-    vincitore();
   }
 };
 
-const vincitore = () => {
+const vincitore = (x: number, y: number) => {
   let randomNumer: number = Math.floor(Math.random() * (100 - 1) + 1);
   estratto.innerHTML = `${randomNumer}`;
-  if (giocatore1 === randomNumer) {
+  if (x === randomNumer) {
     risultato.innerHTML = "Giocatore1 ha indovinato il numero";
-  } else if (giocatore2 === randomNumer) {
+  } else if (y === randomNumer) {
     risultato.innerHTML = "Giocatore2 ha indovinato il numero";
-  } else if (
-    Math.abs(giocatore1 - randomNumer) < Math.abs(giocatore2 - randomNumer)
-  ) {
+  } else if (Math.abs(x - randomNumer) < Math.abs(y - randomNumer)) {
     risultato.innerHTML =
       "Nessuno ha indovinato ma Giocatore 1 si è avvicinato di più";
-  } else if (
-    Math.abs(giocatore2 - randomNumer) < Math.abs(giocatore1 - randomNumer)
-  ) {
+  } else if (Math.abs(y - randomNumer) < Math.abs(x - randomNumer)) {
     risultato.innerHTML =
       "Nessuno ha indovinato ma Giocatore 2 si è avvicinato di più";
   }
@@ -45,7 +37,10 @@ const vincitore = () => {
 
 btn.addEventListener("submit", (event) => {
   event.preventDefault();
-  controlla();
+  const giocatore1: number = Number(input1?.value);
+  const giocatore2: number = Number(input2?.value);
+  controlla(giocatore1, giocatore2);
+  vincitore(giocatore1, giocatore2);
 });
 
 resetto.addEventListener("click", () => {
