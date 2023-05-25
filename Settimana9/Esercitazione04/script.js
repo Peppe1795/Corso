d0dd0f52b66cb96bed74d9551d0f99cecffeaba0
Peppe1795 <petrucci238@gmail.com>
2023-05-25 17:18:29 +0200

    Settimana9 Esercitazione04

--- Settimana9/Esercitazione04/script.js
+++ Settimana9/Esercitazione04/script.js
@@ -49,16 +49,16 @@ var ClothesShop = /** @class */ (function () {
         this.saldo = _saldo;
     }
     ClothesShop.prototype.getsaldoCapo = function () {
-        return ((this.prezzoivainclusa * this.saldo) / 100);
+        return (this.prezzoivainclusa * this.saldo) / 100;
     };
     ClothesShop.prototype.getAcquistoCapo = function () {
-        return (this.prezzoivainclusa - this.getsaldoCapo());
+        return this.prezzoivainclusa - this.getsaldoCapo();
     };
     return ClothesShop;
 }());
 function fetchData() {
     return __awaiter(this, void 0, void 0, function () {
-        var response, data, nuovoCapo, nuovoCapo2, nuovoCapo3, nuovoCapo4, nuovoCapo5, error_1;
+        var response, data, error_1;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
@@ -73,26 +73,12 @@ function fetchData() {
                 case 2:
                     data = _a.sent();
                     console.log("Data fetched", data);
-                    nuovoCapo = new ClothesShop(data[0].id, data[0].codprod, data[0].collezione, data[0].capo, data[0].modello, data[0].quantita, data[0].colore, data[0].prezzoivaesclusa, data[0].prezzoivainclusa, data[0].disponibile, data[0].saldo);
-                    console.log(nuovoCapo);
-                    console.log('Sconto applicato:' + ' ' + '€' + nuovoCapo.getsaldoCapo());
-                    console.log('Costo capo scontato:' + ' ' + '€' + nuovoCapo.getAcquistoCapo());
-                    nuovoCapo2 = new ClothesShop(data[1].id, data[1].codprod, data[1].collezione, data[1].capo, data[1].modello, data[1].quantita, data[1].colore, data[1].prezzoivaesclusa, data[1].prezzoivainclusa, data[1].disponibile, data[1].saldo);
-                    console.log(nuovoCapo2);
-                    console.log('Sconto applicato:' + ' ' + '€' + nuovoCapo2.getsaldoCapo());
-                    console.log('Costo capo scontato:' + ' ' + '€' + nuovoCapo2.getAcquistoCapo());
-                    nuovoCapo3 = new ClothesShop(data[2].id, data[2].codprod, data[2].collezione, data[2].capo, data[2].modello, data[2].quantita, data[2].colore, data[2].prezzoivaesclusa, data[2].prezzoivainclusa, data[2].disponibile, data[2].saldo);
-                    console.log(nuovoCapo3);
-                    console.log('Sconto applicato:' + ' ' + '€' + nuovoCapo3.getsaldoCapo());
-                    console.log('Costo capo scontato:' + ' ' + '€' + nuovoCapo3.getAcquistoCapo());
-                    nuovoCapo4 = new ClothesShop(data[3].id, data[3].codprod, data[3].collezione, data[3].capo, data[3].modello, data[3].quantita, data[3].colore, data[3].prezzoivaesclusa, data[3].prezzoivainclusa, data[3].disponibile, data[3].saldo);
-                    console.log(nuovoCapo4);
-                    console.log('Sconto applicato:' + ' ' + '€' + nuovoCapo4.getsaldoCapo());
-                    console.log('Costo capo scontato:' + ' ' + '€' + nuovoCapo4.getAcquistoCapo());
-                    nuovoCapo5 = new ClothesShop(data[4].id, data[4].codprod, data[4].collezione, data[4].capo, data[4].modello, data[4].quantita, data[4].colore, data[4].prezzoivaesclusa, data[4].prezzoivainclusa, data[4].disponibile, data[4].saldo);
-                    console.log(nuovoCapo5);
-                    console.log('Sconto applicato:' + ' ' + '€' + nuovoCapo5.getsaldoCapo());
-                    console.log('Costo capo scontato:' + ' ' + '€' + nuovoCapo5.getAcquistoCapo());
+                    data.forEach(function (element) {
+                        var nuovoCapo = new ClothesShop(element.id, element.codprod, element.collezione, element.capo, element.modello, element.quantita, element.colore, element.prezzoivaesclusa, element.prezzoivainclusa, element.disponibile, element.saldo);
+                        console.log(nuovoCapo);
+                        console.log("Sconto applicato:" + " " + "€" + nuovoCapo.getsaldoCapo());
+                        console.log("Costo capo scontato:" + " " + "€" + nuovoCapo.getAcquistoCapo());
+                    });
                     return [3 /*break*/, 4];
                 case 3:
                     error_1 = _a.sent();
--- Settimana9/Esercitazione04/script.ts
+++ Settimana9/Esercitazione04/script.ts
@@ -37,11 +37,11 @@ class ClothesShop {
   }
 
   getsaldoCapo(): number {
-    return ((this.prezzoivainclusa * this.saldo) / 100);
+    return (this.prezzoivainclusa * this.saldo) / 100;
   }
 
   getAcquistoCapo(): number {
-    return (this.prezzoivainclusa - this.getsaldoCapo());
+    return this.prezzoivainclusa - this.getsaldoCapo();
   }
 }
 
@@ -53,95 +53,26 @@ async function fetchData(): Promise<any> {
     }
     const data: any = await response.json();
     console.log("Data fetched", data);
-    let nuovoCapo: any = new ClothesShop(
-      data[0].id,
-      data[0].codprod,
-      data[0].collezione,
-      data[0].capo,
-      data[0].modello,
-      data[0].quantita,
-      data[0].colore,
-      data[0].prezzoivaesclusa,
-      data[0].prezzoivainclusa,
-      data[0].disponibile,
-      data[0].saldo
-    );
-
-    console.log(nuovoCapo);
-    console.log('Sconto applicato:'+ ' ' + '€' + nuovoCapo.getsaldoCapo());
-    console.log('Costo capo scontato:'+ ' ' + '€' + nuovoCapo.getAcquistoCapo());
-
-    let nuovoCapo2: any = new ClothesShop(
-        data[1].id,
-        data[1].codprod,
-        data[1].collezione,
-        data[1].capo,
-        data[1].modello,
-        data[1].quantita,
-        data[1].colore,
-        data[1].prezzoivaesclusa,
-        data[1].prezzoivainclusa,
-        data[1].disponibile,
-        data[1].saldo
+    data.forEach((element: any) => {
+      let nuovoCapo: any = new ClothesShop(
+        element.id,
+        element.codprod,
+        element.collezione,
+        element.capo,
+        element.modello,
+        element.quantita,
+        element.colore,
+        element.prezzoivaesclusa,
+        element.prezzoivainclusa,
+        element.disponibile,
+        element.saldo
       );
-  
-      console.log(nuovoCapo2);
-      console.log('Sconto applicato:'+ ' ' + '€' + nuovoCapo2.getsaldoCapo());
-      console.log('Costo capo scontato:'+ ' ' + '€' + nuovoCapo2.getAcquistoCapo());
-
-      let nuovoCapo3: any = new ClothesShop(
-        data[2].id,
-        data[2].codprod,
-        data[2].collezione,
-        data[2].capo,
-        data[2].modello,
-        data[2].quantita,
-        data[2].colore,
-        data[2].prezzoivaesclusa,
-        data[2].prezzoivainclusa,
-        data[2].disponibile,
-        data[2].saldo
+      console.log(nuovoCapo);
+      console.log("Sconto applicato:" + " " + "€" + nuovoCapo.getsaldoCapo());
+      console.log(
+        "Costo capo scontato:" + " " + "€" + nuovoCapo.getAcquistoCapo()
       );
-  
-      console.log(nuovoCapo3);
-      console.log('Sconto applicato:'+ ' ' + '€' + nuovoCapo3.getsaldoCapo());
-      console.log('Costo capo scontato:'+ ' ' + '€' + nuovoCapo3.getAcquistoCapo());
-
-      let nuovoCapo4: any = new ClothesShop(
-        data[3].id,
-        data[3].codprod,
-        data[3].collezione,
-        data[3].capo,
-        data[3].modello,
-        data[3].quantita,
-        data[3].colore,
-        data[3].prezzoivaesclusa,
-        data[3].prezzoivainclusa,
-        data[3].disponibile,
-        data[3].saldo
-      );
-  
-      console.log(nuovoCapo4);
-      console.log('Sconto applicato:'+ ' ' + '€' + nuovoCapo4.getsaldoCapo());
-      console.log('Costo capo scontato:'+ ' ' + '€' + nuovoCapo4.getAcquistoCapo());
-
-      let nuovoCapo5: any = new ClothesShop(
-        data[4].id,
-        data[4].codprod,
-        data[4].collezione,
-        data[4].capo,
-        data[4].modello,
-        data[4].quantita,
-        data[4].colore,
-        data[4].prezzoivaesclusa,
-        data[4].prezzoivainclusa,
-        data[4].disponibile,
-        data[4].saldo
-      );
-  
-      console.log(nuovoCapo5);
-      console.log('Sconto applicato:'+ ' ' + '€' + nuovoCapo5.getsaldoCapo());
-      console.log('Costo capo scontato:'+ ' ' + '€' + nuovoCapo5.getAcquistoCapo());
+    });
   } catch (error: any) {
     console.log("Fetch Error:", error.message);
   }
