a4b462701079acec09351b961398201ab6067a18
Peppe1795 <petrucci238@gmail.com>
2023-06-12 12:47:16 +0200

    ProgettoSettimana11

--- Settimana11/ProgettoSettimana11/proggettoSettimana11/src/app/auth/auth.service.ts
+++ Settimana11/ProgettoSettimana11/proggettoSettimana11/src/app/auth/auth.service.ts
@@ -8,98 +8,95 @@ import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class AuthService {
-    jwtHelper = new JwtHelperService();
-    baseURL = environment.baseURL;
-    private authSubj = new BehaviorSubject<null | Data>(null);
-    utente!: Data;
+  jwtHelper = new JwtHelperService();
+  baseURL = environment.baseURL;
+  private authSubj = new BehaviorSubject<null | Data>(null);
+  utente!: Data;
 
-    user$ = this.authSubj.asObservable();
-    timeoutLogout: any;
+  user$ = this.authSubj.asObservable();
+  timeoutLogout: any;
 
-    constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
-    login(data: { email: string; password: string }) {
-      return this.http.post<Data>(`${this.baseURL}login`, data).pipe(
-        tap((data) => {
-          this.authSubj.next(data);
-          this.utente = data;
-          console.log(this.utente);
-          localStorage.setItem('user', JSON.stringify(data));
-          this.autoLogout(data);
-        }),
-        catchError(this.errors)
-      );
+  login(data: { email: string; password: string }) {
+    return this.http.post<Data>(`${this.baseURL}login`, data).pipe(
+      tap((data) => {
+        this.authSubj.next(data);
+        this.utente = data;
+        console.log(this.utente);
+        localStorage.setItem('user', JSON.stringify(data));
+        this.autoLogout(data);
+      }),
+      catchError(this.errors)
+    );
+  }
+  restore() {
+    const user = localStorage.getItem('user');
+    if (!user) {
+      return;
     }
-    restore() {
-
-        const user = localStorage.getItem('user');
-        if (!user) {
-            return;
-        }
-        const userData: Data = JSON.parse(user);
-        if (this.jwtHelper.isTokenExpired(userData.accessToken)) {
-
-            return;
-        }
-        this.authSubj.next(userData);
-        this.autoLogout(userData);
+    const userData: Data = JSON.parse(user);
+    if (this.jwtHelper.isTokenExpired(userData.accessToken)) {
+      return;
     }
+    this.authSubj.next(userData);
+    this.autoLogout(userData);
+  }
 
-    signup(data: {
-        nome: string;
-        cognome: string;
-        email: string;
-        password: string;
-    }) {
-        return this.http.post(`${this.baseURL}register`, data);
+  signup(data: {
+    nome: string;
+    cognome: string;
+    email: string;
+    password: string;
+  }) {
+    return this.http.post(`${this.baseURL}register`, data);
+  }
+
+  logout() {
+    this.authSubj.next(null);
+    localStorage.removeItem('user');
+    this.router.navigate(['/login']);
+    if (this.timeoutLogout) {
+      clearTimeout(this.timeoutLogout);
     }
+  }
 
-    logout() {
-        this.authSubj.next(null);
-        localStorage.removeItem('user');
-        this.router.navigate(['/login']);
-        if (this.timeoutLogout) {
+  autoLogout(data: Data) {
+    const expirationDate = this.jwtHelper.getTokenExpirationDate(
+      data.accessToken
+    ) as Date;
+    const expirationMilliseconds =
+      expirationDate.getTime() - new Date().getTime();
+    this.timeoutLogout = setTimeout(() => {
+      this.logout();
+    }, expirationMilliseconds);
+  }
 
-            clearTimeout(this.timeoutLogout);
-        }
+  private errors(err: any) {
+    switch (err.error) {
+      case 'Email already exists':
+        return throwError('Utente già presente');
+        break;
+
+      case 'Email format is invalid':
+        return throwError('Formato mail non valido');
+        break;
+
+      default:
+        return throwError('Errore nella chiamata');
+        break;
     }
+  }
 
-    autoLogout(data: Data) {
-        const expirationDate = this.jwtHelper.getTokenExpirationDate(
-            data.accessToken
-        ) as Date;
-        const expirationMilliseconds =
-            expirationDate.getTime() - new Date().getTime();
-        this.timeoutLogout = setTimeout(() => {
-            this.logout();
-        }, expirationMilliseconds);
+  getCurrentUserId(): number | null {
+    const user = localStorage.getItem('user');
+    if (user) {
+      const userData: Data = JSON.parse(user);
+      return userData.user.id;
     }
-
-    private errors(err: any) {
-        switch (err.error) {
-            case 'Email already exists':
-                return throwError('Utente già presente');
-                break;
-
-            case 'Email format is invalid':
-                return throwError('Formato mail non valido');
-                break;
-
-            default:
-                return throwError('Errore nella chiamata');
-                break;
-        }
-    }
-
-    getCurrentUserId(): number | null {
-      const user = localStorage.getItem('user');
-      if (user) {
-        const userData: Data = JSON.parse(user);
-        return userData.user.id;
-      }
-      return null;
-    }
+    return null;
+  }
 }
--- Settimana11/ProgettoSettimana11/proggettoSettimana11/src/app/auth/singin/singin.component.ts
+++ Settimana11/ProgettoSettimana11/proggettoSettimana11/src/app/auth/singin/singin.component.ts
@@ -3,35 +3,32 @@ import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 
-
 @Component({
   selector: 'app-singin',
   templateUrl: './singin.component.html',
-  styleUrls: ['./singin.component.scss']
+  styleUrls: ['./singin.component.scss'],
 })
 export class SinginComponent implements OnInit {
-
   isLoading = false;
 
-    constructor(private authSrv: AuthService, private router: Router) {}
+  constructor(private authSrv: AuthService, private router: Router) {}
 
-    ngOnInit(): void {}
+  ngOnInit(): void {}
 
-    registra(form: NgForm) {
-        this.isLoading = true;
-        console.log(form.value);
-        try {
-            this.authSrv.signup(form.value).subscribe();
-            this.router.navigate(['/login']);
-            this.isLoading = false
-        } catch (error: any) {
-            console.error(error);
-            if (error.status == 400) {
-                alert('Email già registrata!');
-                this.router.navigate(['/register']);
-            }
-            this.isLoading = false
-        }
+  registra(form: NgForm) {
+    this.isLoading = true;
+    console.log(form.value);
+    try {
+      this.authSrv.signup(form.value).subscribe();
+      this.router.navigate(['/login']);
+      this.isLoading = false;
+    } catch (error: any) {
+      console.error(error);
+      if (error.status == 400) {
+        alert('Email già registrata!');
+        this.router.navigate(['/register']);
+      }
+      this.isLoading = false;
     }
-
+  }
 }
